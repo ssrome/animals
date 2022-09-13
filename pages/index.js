03e40fb8ec4e32e8152efc4d36f09125de5cdf92
Sabrina <sabrina.samuel@madetech.com>
2022-09-13 15:51:12 +0100

    Update index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Heading from "../src/components/Heading";
+import Animal from "../src/components/Animal";
 
 function HomePage() {
   return (
     <div>
       <Heading />
+      <Animal />
     </div>
   );
 }
