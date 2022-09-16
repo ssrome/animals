c3fe41ea107900dacb4b7bae22e87bd7e5edbc50
Sabrina <sabrina.samuel@madetech.com>
2022-09-16 12:21:35 +0100

    Add styles to animal component

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Name from "./Name";
+import styles from "./Animal.module.css";
 
 export default function Animal() {
-  const animalData = {
-    name: "Siamang",
-    animalType: "Mammal",
-    lifespan: 23,
-    habitat: "Tropical rainforest",
-    diet: "Primarily fruit and leaves, some invertebrates",
-    imageLink:
-      "https://upload.wikimedia.org/wikipedia/commons/a/a4/DPPP_5348.jpg",
-  };
   return (
     <>
-      <Name name={animalData.name} />
-      <img
-        src={animalData.imageLink}
-        alt="picture of a siamang"
-        height={200}
-        width={300}
-      />
-      <div>Type: {animalData.animalType}</div>
-      <div>Lifespan: {animalData.lifespan}</div>
-      <div>Habitat: {animalData.habitat}</div>
-      <div>Diet: {animalData.diet}</div>
+      <Name name="Siamang" />
+      <div className={styles.imgContainer}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a4/DPPP_5348.jpg"
+          alt="picture of a siamang"
+          height={200}
+          width={300}
+          className={styles.animalImg}
+        />
+      </div>
+      <div className={styles.infoContainer}>
+        <ul className={styles.info}>
+          <li>Type: Mammal</li>
+          <li>Lifespan: 23</li>
+          <li>Habitat: Tropical rainforest</li>
+          <li>Diet: Primarily fruit and leaves, some invertebrates</li>
+        </ul>
+      </div>
     </>
   );
 }
