8d74c2d7969f896398ac2c84c623bfa3c77bba5b
Sabrina <sabrina.samuel@madetech.com>
2022-09-20 15:54:55 +0100

    Add fetch animal from api and tests

--- __tests__/unit/Name.test.jsx
+++ __tests__/unit/Name.test.jsx
@@ -3,11 +3,18 @@ import { render, screen } from "@testing-library/react";
 import Name from "../../src/components/Name";
 
 describe("Name component", () => {
+  beforeEach(() => {
+    render(<Name name="Plains Zebra" latinName="Equus quagga" />);
+  });
   it("shows the name of the animal", async () => {
-    render(<Name name="Red Panda" />);
-
-    const nameText = screen.getByText(/red panda/i);
+    const nameText = screen.getByText(/plains zebra/i);
 
     expect(nameText).toBeInTheDocument();
+  });
+
+  it("shows the latin name", async () => {
+    const latinText = screen.getByText(/equus quagga/i);
+
+    expect(latinText).toBeInTheDocument();
   });
 });
---
+++ __tests__/unit/utils/getAnimal.test.js
@@ -0,0 +1,38 @@
+import getRandomAnimal from "../../../utils/getAnimals";
+
+const ANIMAL = {
+  name: "Plains Zebra",
+  latin_name: "Equus quagga",
+  animal_type: "Mammal",
+  active_time: "Diurnal",
+  length_min: "7",
+  length_max: "8",
+  weight_min: "385",
+  weight_max: "850",
+  lifespan: "15",
+  habitat: "Savannah and woodland",
+  diet: "Grass",
+  geo_range: "Eastern and Southern Africa",
+  image_link:
+    "https://upload.wikimedia.org/wikipedia/commons/4/45/Equus_quagga_burchellii_-_Etosha%2C_2014.jpg",
+  id: 137,
+};
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(ANIMAL),
+  })
+);
+
+let animal;
+
+describe("Random animal", () => {
+  beforeEach(async () => {
+    animal = await getRandomAnimal();
+  });
+
+  it("should fetch an animal", () => {
+    expect(animal).not.toBeNull();
+    expect(fetch).toHaveBeenCalledTimes(1);
+  });
+});
