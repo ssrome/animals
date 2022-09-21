e5b059b5e8aab7f61eaf1ff6c82dc80c29c55d4c
Sabrina <sabrina.samuel@madetech.com>
2022-09-21 12:18:55 +0100

    Add baseUrl to cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   downloadsFolder: "__tests__/downloads",
   e2e: {
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
