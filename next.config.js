14a2c6d5528e0830ce30c61ed236816cffd092f7
Sabrina <sabrina.samuel@madetech.com>
2022-10-04 09:52:33 +0100

    Add cloudinary url to next.confg. Remove other urls

--- next.config.js
+++ next.config.js
@@ -3,18 +3,9 @@ module.exports = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "upload.wikimedia.org",
-        pathname: "/wikipedia/commons/**",
-      },
-      {
-        protocol: "https",
-        hostname: "static.wikia.nocookie.net",
-        pathname: "/animals/**",
-      },
-      {
-        protocol: "https",
-        hostname: "americanminipigassociation.com",
-        pathname: "/wp-content/**",
+        hostname: "res.cloudinary.com",
+
+        pathname: "/dghscys28/image/fetch/**",
       },
     ],
   },
