9c9f2018f619a67fe33aec66a28baeea1fd5e7e4
Sabrina <sabrina.samuel@madetech.com>
2022-10-04 10:43:30 +0100

    Add cloudinary loader and change image properties

--- src/components/Animal.jsx
+++ src/components/Animal.jsx
@@ -1,21 +1,39 @@
 import React from "react";
+import Image from "next/future/image";
 import Name from "./Name";
 import styles from "./Animal.module.css";
 
 export default function Animal({ animal }) {
   if (animal) {
+    function animalImageLoader({ src, width, quality }) {
+      return `https://res.cloudinary.com/dghscys28/image/fetch/${src}?w=${width}&q=${
+        quality || 25
+      }`;
+    }
+
+    const animalImage = (props) => {
+      return (
+        <Image
+          loader={animalImageLoader}
+          src={animal.image_link}
+          alt={animal.name}
+          fill
+          className={styles.animalImg}
+          priority
+          placeholder="blur"
+          blurDataURL="/images/placeholder.png"
+          sizes="(max-width: 425px) 100vw,
+              (max-width: 375px) 50vw,
+              33vw"
+        />
+      );
+    };
+
     return (
       <>
         <Name name={animal.name} latinName={animal.latin_name} />
-        <div className={styles.imgContainer}>
-          <img
-            src={animal.image_link}
-            alt={animal.name}
-            height={235}
-            width={314}
-            className={styles.animalImg}
-          />
-        </div>
+
+        <div className={styles.imgContainer}>{animalImage()}</div>
         <div className={styles.infoContainer}>
           <ul className={styles.info} data-cy="animal-info-list">
             <li>Type: {animal.animal_type}</li>
